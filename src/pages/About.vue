4cfaa5a97f487e44e6e1f58ae37bf17ba95f77af
peterkovacik <peter.a.kovacik@gmail.com>
2023-07-04 00:22:26 -0500

    revise About page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,6 +2,20 @@
   <Layout>
     <h1 class="text-center title text-green-900 text-5xl pt-8 pb-3">About Omar</h1>
     <div class="content-width px-7">
+      <p class="pt-3 pb-4">
+        Omar became Muslim in 2001 and began studying Classical Arabic in 2005 at Qasid Institute in Jordan. He has a Bachelor of Arts in Linguistics from the University of Wisconsin-Milwaukee and certificates in Teaching English as a Foreign Language from the Wisconsin ESL Institute (WESLI) in Madison, WI. He taught English to non-native speakers in Aden, Yemen and Bursa, Turkey, he taught Arabic and ESL at Clara Mohammed School in Milwaukee, WI, and he designed and taught a program for Classical Arabic at Fox Valley Islamic Society in Neenah, WI. 
+      </p>
+      <figure class="pt-3 pb-5">
+          <img src="/images/self-image-trees.avif" alt="image of Omar"  class="center-image self-image"> 
+      </figure>
+      <p class="pt-3 pb-1">
+        At Qasid Institute, he completed most of the program in Classical Arabic and took private lessons in Arabic grammar with Ustadh Amer Melham. He continued his Arabic studies with Dr. Hamid Ouali (University of Wisconsin-Milwaukee), Mufti Yusuf Mullan (Toronto Shariah Program), Mawlana Syed Javed Ali Shah (private lessons), and various other Arabic instructors.
+      </p>
+      <p class="pt-3 pb-9">
+        He currently resides with his family in Green Bay, WI and is pursuing a Masters of Divinity in Islamic Chaplaincy at The Islamic Seminary of America (TISA) while taking additional coursework with Shaykh Hamza Karamali (Basira Education), Shaykh Mujahid Abdul-Karim (Fayda Institute), and Shaykh Saifullah Abu M. (Al-Madrasah Al-Mālikiyyah).
+      </p>
+
+      <!--
         <p class="pt-3 pb-4">
           Omar became Muslim in 2001 and began studying Classical Arabic in 2005 at Qasid Institute in Jordan. While there, he   completed most of the program in Classical Arabic and took private lessons in Arabic grammar with Ustadh Amer Melham. He continued his Arabic studies with various instructors including Dr. Hamid Ouali (University of Wisconsin-Milwaukee), Mufti Yusuf Mullan (Toronto Shariah Program), and Mawlana Syed Javed Ali Shah (private lessons). 
         </p>
@@ -9,9 +23,9 @@
             <img src="/images/self-image-trees.avif" alt="image of Omar"  class="center-image self-image"> 
         </figure>
         <p class="pt-3 pb-9">
-          Omar has a Bachelor of Arts in Linguistics from the University of Wisconsin-Milwaukee and certificates in Teaching English as a Foreign Language from the Wisconsin ESL Institute (WESLI) in Madison, WI. He taught English to non-native speakers in Aden, Yemen and Bursa, Turkey, he taught Arabic and ESL at Clara Mohammed School in Milwaukee, WI, and he designed and taught a program for Classical Arabic at Fox Valley Islamic Society in Neenah, WI. He is currently resides with his family in Green Bay, WI and is pursuing a Masters of Divinity in Islamic Chaplaincy at The Islamic Seminary of America (TISA) while  taking additional coursework at Basira Education, Fayda Institute, and the Maliki School.
+          Omar has a Bachelor of Arts in Linguistics from the University of Wisconsin-Milwaukee and certificates in Teaching English as a Foreign Language from the Wisconsin ESL Institute (WESLI) in Madison, WI. He taught English to non-native speakers in Aden, Yemen and Bursa, Turkey, he taught A rabic and ESL at Clara Mohammed School in Milwaukee, WI, and he designed and taught a program for Classical Arabic at Fox Valley Islamic Society in Neenah, WI. He is currently resides with his family in Green Bay, WI and is pursuing a Masters of Divinity in Islamic Chaplaincy at The Islamic Seminary of America (TISA) while  taking additional coursework at Basira Education, Fayda Institute, and the Maliki School.
         </p>
-        
+       --> 
         
         
         <!--
