062254fba8e1c29e8035d4ab0e1b659e499a70c0
Peter Kovacik <peter.a.kovacik@gmail.com>
2024-04-02 02:44:34 -0500

    change to courses button

--- src/components/CoursesButton.vue
+++ src/components/CoursesButton.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="pt-5 pb-7">
-        <div class="relative w-fit group block mx-auto p-3"> 
+        <div class="relative w-fit group block mx-auto p-1"> 
             <div
-                class="absolute transitiona-all duration-1000 opacity-40 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg filter group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 ">
+                class="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg filter group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 ">
             </div>
             <g-link to="https://arabicwithomar.teachable.com" target="_blank" title="" role="button"
                 class="relative inline-flex items-center justify-center px-5 py-2 text-base font-bold text-white transition-all duration-200 bg-gray-900 border-2 border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 hover:bg-gray-600 rounded">
