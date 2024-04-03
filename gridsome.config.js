68921118f98aacd48e4debff12e707ca1425ef59
Peter Kovacik <peter.a.kovacik@gmail.com>
2024-04-03 05:49:54 -0500

    change site name to Classical Arabic with Omar

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Arabic Instructor',
+  siteName: 'Classical Arabic with Omar',
   plugins: [
     {
       use: "gridsome-plugin-tailwindcss2",
